23dafe7527787313306794c2f2fc3e5ad4dc1d1a
Utente <Utente@DESKTOP-P2EKJ3E.station>
2020-05-05 10:04:04 +0200

    Aggiunta nuova stampa

--- ProvaGit/src/it/begear/corso/repository/Prova.java
+++ ProvaGit/src/it/begear/corso/repository/Prova.java
@@ -5,6 +5,8 @@ public class Prova {
 	public static void main(String[] args) {
 		
 		System.out.println("Il mio primo test in team!");
+		
+		System.out.println("La mia prima modifica seria!");
 
 	}
 
